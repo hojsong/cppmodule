dd96cb989af446653531683f4e9d9b59ae4a73c0
Hojo Song <hojsong@cx1r5s4.42seoul.kr>
2023-04-04 08:56:34 +0900

    1

---
+++ module04/ex00/WrongCat.hpp
@@ -0,0 +1,10 @@
+#include "WrongAnimal.hpp"
+
+class WrongCat : public WrongAnimal{
+    public :
+        WrongCat(void);
+        WrongCat(const WrongCat &obj);
+	    WrongCat& operator=(const WrongCat &obj);
+	    ~WrongCat(void);
+	    void makeSound(void) const;
+};
--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -1,21 +1,30 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
     i->makeSound(); //will output the cat sound!
     j->makeSound();
     meta->makeSound();
-    
+
+    const WrongAnimal* w = new WrongAnimal();
+    const WrongAnimal* z = new WrongCat();
+    std::cout << w->getType() << " " << std::endl;
+    std::cout << z->getType() << " " << std::endl;
+
 	delete meta;
 	delete j;
 	delete i;
+    delete w;
+    delete z;
 
     return 0;
 }
