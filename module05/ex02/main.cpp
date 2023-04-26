f181a098705e8c99dba69b0969c593fdda795fad
Hojo Song <hojsong@cx1r2s4.42seoul.kr>
2023-04-26 17:46:49 +0900

    2

--- module05/ex02/main.cpp
+++ module05/ex02/main.cpp
@@ -11,20 +11,20 @@ int main()
 	RobotomyRequestForm form("home");
 	// PresidentialPardonForm form("home");
     // ShrubberyCreationForm form("home");
-
+	std::cout << "---------- 1 TRY -------------" << std::endl;
     try {
         bureaucrat.executeForm(form);
     } catch (const std::exception &e) {
         std::cout << "Exception caught: " << e.what() << std::endl;
     }
-
+	std::cout << "---------- 2 TRY -------------" << std::endl;
     try {
         bureaucrat.signForm(form);
         bureaucrat.setGrade(149);
         bureaucrat.executeForm(form);
     } catch (const std::exception &e) {
     }
-
+	std::cout << "---------- 3 TRY -------------" << std::endl;
     try {
         bureaucrat.setGrade(1);
 		bureaucrat.signForm(form);
